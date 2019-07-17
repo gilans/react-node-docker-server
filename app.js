c61bca74f91bc7aa8198db8928274d0798196f82
suarezgilberto <suarezgilberto@hotmail.com>
2019-07-16 21:21:45 -0400

    deploy

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var testAPIRouter = require("./routes/testAPI");
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
